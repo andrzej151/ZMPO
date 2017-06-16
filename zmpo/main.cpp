6595a4fb5241aab17b6dc3568a33387fed5ad05d
Andrzej-dell <andrzej.dabrowski94@gmail.com>
2017-06-16 10:51:50 +0200

    zm

--- zmpo/main.cpp
+++ zmpo/main.cpp
@@ -1,17 +1,27 @@
+#include <iostream>
 #include <stdio.h>
 #include <conio.h>
 
+using namespace std;
 main()
 {
-   int x, y;
+long kwota[] = { 120, 1650000, 5200, 1490000123 };
+for( int i = 0; i < 4; i++ )
+{
+    cout << "Rachunek nr ";
+    cout.width( 2 );
+    cout.fill( '*' );
+    cout << i << " opiewa na sume :";
+    cout.width( 10 );
+    cout.fill( ' ' );
+    cout << kwota[ i ] << " DM\n";
+}
 
-   x = 10;
-   y = 10;
-
-   gotoxy(x, y);
-
-   printf("C program to change cursor position.");
-
-   getch();
-   return 0;
+int i = 31;
+cout << i << endl; //31
+cout.setf( ios::hex, ios::basefield );
+cout << i << endl; //1F
+cout << hex << i << endl; //1F - drugi sposób (16)
+cout << oct << i << endl; //37 - drugi sposób (8)
+cout.setf( ios::dec, ios::basefield );
 }
