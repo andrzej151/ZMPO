b751cb638b0a127dc46078df15a698f9b94af87a
Andrzej-dell <andrzej.dabrowski94@gmail.com>
2017-06-20 16:12:16 +0200

    po rozmowie

--- zmpo/src/main.cpp
+++ zmpo/src/main.cpp
@@ -2,13 +2,17 @@
 
 main::main()
 {
-    int x, y;
+/*    int x, y;
     x = 10;
     y = 10;
     gotoxy(x, y);
     printf("C program to change cursor position.");
     getch();
-    return 0;
+    return 0;*/
+    string hit = _getch();
+        switch (hit) {
+        case 'W': ... break;
+        case 13 : ... break;}
 }
 
 main::~main()
